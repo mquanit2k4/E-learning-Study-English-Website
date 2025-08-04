changchan255/ruby_naitei22-sjp_e-learning-system

Initial database

app/models/component.rb
@@ -1,0 +1,11 @@
+class Component < ApplicationRecord
+  belongs_to :lesson
+  # `optional: true` vì một component có thể là test, word, hoặc paragraph
+  belongs_to :test, optional: true
+  belongs_to :word, optional: true
+
+  has_many :user_words, dependent: :destroy
+  has_many :test_results, dependent: :destroy
+
+  enum type: { word: 0, test: 1, paragraph: 2 }
+end

app/models/question.rb
@@ -1,0 +1,6 @@
+class Question < ApplicationRecord
+  belongs_to :test
+  has_many :answers, dependent: :destroy
+
+  enum type: { single_choice: 0, multiple_choice: 1 }
+end

db/schema.rb
@@ -10,29 +10,163 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_07_24_020258) do
-  create_table "microposts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2025_08_04_100003) do
+  create_table "admin_course_managers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "course_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_admin_course_managers_on_course_id"
+    t.index ["user_id"], name: "index_admin_course_managers_on_user_id"
+  end
+
+  create_table "answers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "question_id", null: false
+    t.string "content", null: false
+    t.boolean "correct", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "components", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "lesson_id", null: false
+    t.integer "type", null: false
+    t.bigint "test_id"
+    t.bigint "word_id"
     t.text "content"
+    t.bigint "index_in_lesson", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lesson_id"], name: "index_components_on_lesson_id"
+    t.index ["test_id"], name: "index_components_on_test_id"
+    t.index ["word_id"], name: "index_components_on_word_id"
+  end
+
+  create_table "courses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.bigint "created_by_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["created_by_id"], name: "index_courses_on_created_by_id"
+  end
+
+  create_table "lessons", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "course_id", null: false
+    t.string "title", null: false
+    t.text "description", null: false
+    t.integer "position", null: false
+    t.bigint "created_by_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_lessons_on_course_id"
+    t.index ["created_by_id"], name: "index_lessons_on_created_by_id"
+  end
+
+  create_table "questions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "test_id", null: false
+    t.text "content", null: false
+    t.integer "type", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
+  end
+
+  create_table "test_results", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "component_id", null: false
+    t.integer "attempt_number", null: false
+    t.json "user_answers", null: false
+    t.integer "mark", null: false
+    t.integer "status", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["component_id"], name: "index_test_results_on_component_id"
+    t.index ["user_id"], name: "index_test_results_on_user_id"
+  end
+
+  create_table "tests", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "name", null: false
+    t.text "description", null: false
+    t.integer "duration", null: false
+    t.integer "max_attempts", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
+  create_table "user_courses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "course_id", null: false
+    t.integer "enrolment_status", default: 0, null: false
+    t.integer "progress", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_user_courses_on_course_id"
+    t.index ["user_id"], name: "index_user_courses_on_user_id"
+  end
+
+  create_table "user_lessons", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "lesson_id", null: false
+    t.integer "status", default: 0, null: false
+    t.integer "grade", null: false
+    t.datetime "completed_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lesson_id"], name: "index_user_lessons_on_lesson_id"
+    t.index ["user_id"], name: "index_user_lessons_on_user_id"
+  end
+
+  create_table "user_words", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "component_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["component_id"], name: "index_user_words_on_component_id"
+    t.index ["user_id"], name: "index_user_words_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
+    t.string "name", limit: 100
+    t.string "email", limit: 100, null: false
+    t.string "password_digest", null: false
     t.date "birthday"
+    t.integer "gender"
+    t.integer "role", default: 0, null: false
+    t.string "remember_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "gender"
-    t.string "remember_digest"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
   end
 
+  create_table "words", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "content", null: false
+    t.text "meaning", null: false
+    t.integer "type", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "admin_course_managers", "courses"
+  add_foreign_key "admin_course_managers", "users"
+  add_foreign_key "answers", "questions"
+  add_foreign_key "components", "lessons"
+  add_foreign_key "components", "tests"
+  add_foreign_key "components", "words"
+  add_foreign_key "courses", "users", column: "created_by_id"
+  add_foreign_key "lessons", "courses"
+  add_foreign_key "lessons", "users", column: "created_by_id"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "test_results", "components"
+  add_foreign_key "test_results", "users"
+  add_foreign_key "user_courses", "courses"
+  add_foreign_key "user_courses", "users"
+  add_foreign_key "user_lessons", "lessons"
+  add_foreign_key "user_lessons", "users"
+  add_foreign_key "user_words", "components"
+  add_foreign_key "user_words", "users"
 end

mquanit2k4/E-learning-Study-English-Website

app/models/component.rb
@@ -1,0 +1,10 @@
+class Component < ApplicationRecord
+  belongs_to :lesson
+  belongs_to :test, optional: true
+  belongs_to :word, optional: true
+
+  has_many :user_words, dependent: :destroy
+  has_many :test_results, dependent: :destroy
+
+  enum component_type: {word: 0, test: 1, paragraph: 2}
+end

app/models/question.rb
@@ -1,0 +1,7 @@
+class Question < ApplicationRecord
+  belongs_to :test
+
+  has_many :answers, dependent: :destroy
+
+  enum question_type: {single_choice: 0, multiple_choice: 1}
+end

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_08_01_042718) do
+ActiveRecord::Schema[7.0].define(version: 2025_08_04_100004) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,53 +39,164 @@ ActiveRecord::Schema[7.0].define(version: 2025_08_01_042718) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "microposts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "admin_course_managers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
-    t.index ["user_id"], name: "index_microposts_on_user_id"
+    t.bigint "course_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_admin_course_managers_on_course_id"
+    t.index ["user_id"], name: "index_admin_course_managers_on_user_id"
   end
 
-  create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
+  create_table "answers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "question_id", null: false
+    t.string "content", null: false
+    t.boolean "correct", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "components", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "lesson_id", null: false
+    t.integer "component_type", null: false
+    t.bigint "test_id"
+    t.bigint "word_id"
+    t.text "content"
+    t.bigint "index_in_lesson", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lesson_id"], name: "index_components_on_lesson_id"
+    t.index ["test_id"], name: "index_components_on_test_id"
+    t.index ["word_id"], name: "index_components_on_word_id"
+  end
+
+  create_table "courses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.bigint "created_by_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["created_by_id"], name: "index_courses_on_created_by_id"
+  end
+
+  create_table "lessons", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "course_id", null: false
+    t.string "title", null: false
+    t.text "description", null: false
+    t.integer "position", null: false
+    t.bigint "created_by_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_lessons_on_course_id"
+    t.index ["created_by_id"], name: "index_lessons_on_created_by_id"
+  end
+
+  create_table "questions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "test_id", null: false
+    t.text "content", null: false
+    t.integer "question_type", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
+  end
+
+  create_table "test_results", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "component_id", null: false
+    t.integer "attempt_number", null: false
+    t.json "user_answers", null: false
+    t.integer "mark", null: false
+    t.integer "status", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["component_id"], name: "index_test_results_on_component_id"
+    t.index ["user_id"], name: "index_test_results_on_user_id"
+  end
+
+  create_table "tests", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "name", null: false
+    t.text "description", null: false
+    t.integer "duration", null: false
+    t.integer "max_attempts", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "relationships", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
+  create_table "user_courses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "course_id", null: false
+    t.integer "enrolment_status", default: 0, null: false
+    t.integer "progress", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["followed_id"], name: "index_relationships_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["course_id"], name: "index_user_courses_on_course_id"
+    t.index ["user_id"], name: "index_user_courses_on_user_id"
+  end
+
+  create_table "user_lessons", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "lesson_id", null: false
+    t.integer "status", default: 0, null: false
+    t.integer "grade", null: false
+    t.datetime "completed_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lesson_id"], name: "index_user_lessons_on_lesson_id"
+    t.index ["user_id"], name: "index_user_lessons_on_user_id"
+  end
+
+  create_table "user_words", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "component_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["component_id"], name: "index_user_words_on_component_id"
+    t.index ["user_id"], name: "index_user_words_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
+    t.string "name", limit: 100
+    t.string "email", limit: 100, null: false
+    t.string "password_digest", null: false
     t.date "birthday"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "gender"
+    t.integer "role", default: 0, null: false
     t.string "remember_digest"
-    t.boolean "admin", default: false
-    t.string "activation_digest"
-    t.boolean "activated", default: false
-    t.datetime "activated_at"
-    t.string "reset_digest"
-    t.datetime "reset_sent_at"
     t.string "provider"
     t.string "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
   end
 
+  create_table "words", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "content", null: false
+    t.text "meaning", null: false
+    t.integer "word_type", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "microposts", "users"
+  add_foreign_key "admin_course_managers", "courses"
+  add_foreign_key "admin_course_managers", "users"
+  add_foreign_key "answers", "questions"
+  add_foreign_key "components", "lessons"
+  add_foreign_key "components", "tests"
+  add_foreign_key "components", "words"
+  add_foreign_key "courses", "users", column: "created_by_id"
+  add_foreign_key "lessons", "courses"
+  add_foreign_key "lessons", "users", column: "created_by_id"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "test_results", "components"
+  add_foreign_key "test_results", "users"
+  add_foreign_key "user_courses", "courses"
+  add_foreign_key "user_courses", "users"
+  add_foreign_key "user_lessons", "lessons"
+  add_foreign_key "user_lessons", "users"
+  add_foreign_key "user_words", "components"
+  add_foreign_key "user_words", "users"
 end